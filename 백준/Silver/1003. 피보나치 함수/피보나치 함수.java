f7b67eca5be5ed1d77ea5180c8dec323b3e7baec
son0925 <155799653+son0925@users.noreply.github.com>
2024-07-27 16:27:36 +0900

    [Silver III] Title: 피보나치 함수, Time: 236 ms, Memory: 18460 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -5,35 +5,31 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner in = new Scanner(System.in);
-		int count = Integer.parseInt(in.nextLine());
+		int len = Integer.parseInt(in.nextLine());
 		
-		for (int i = 0; i < count; i++) {
+		// 피보나치 배열 생성
+		int[] piboArr = new int[41];
+		piboArr[0] = 0;
+		piboArr[1] = 1;
+		for (int i = 2; i <= 40; i++) {
+			piboArr[i] = piboArr[i-1] + piboArr[i-2];
+		}
+		
+		
+		for (int i = 0; i < len; i++) {
 			int n = Integer.parseInt(in.nextLine());
-			int zero = zeroCountPibo(1, 0, n);
-			int one = oneCountPibo(0, 1, n);
-			System.out.println(zero + " " + one);
+			
+			if (n == 0) {
+				System.out.println(piboArr[1] + " " + piboArr[0]);
+				continue;
+			}
+			else if (n == 1) {
+				System.out.println(piboArr[0] + " " + piboArr[1]);
+				continue;
+			}
+			System.out.println(piboArr[n-1] + " " + piboArr[n]);
 		}
 	}
 	
-	public static int zeroCountPibo(int lastNum, int num, int n) {
-		if (n == 0) {
-			return lastNum;
-		}
-		
-		else if (n == 1) {
-			return num;
-		}
-		
-		return zeroCountPibo(num, lastNum+num, n-1);
-	}
-	public static int oneCountPibo(int lastNum, int num, int n) {
-		if (n == 0) {
-			return lastNum;
-		}
-		else if (n == 1) {
-			return num;
-		}
-		
-		return oneCountPibo(num, lastNum+num, n-1);
-	}
+	
 }
