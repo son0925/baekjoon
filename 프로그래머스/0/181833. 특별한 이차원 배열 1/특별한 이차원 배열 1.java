f80d5cc8975006980da0ea34c88e35eb1f3a45e1
son0925 <155799653+son0925@users.noreply.github.com>
2024-07-28 15:56:56 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.25 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
@@ -1,13 +1,19 @@
 class Solution {
     public int[][] solution(int n) {
+        // n x n 크기의 2차원 배열을 생성
         int[][] result = new int[n][n];
+        
+        // 각 행과 열을 반복하면서
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
+                // 행과 열이 같으면 주대각선이므로 1을 대입
                 if (i == j) {
                     result[i][j] = 1;
                 }
             }
         }
+        
+        // 생성된 단위 행렬을 반환
         return result;
     }
 }
