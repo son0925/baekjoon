79634cd7c5265208139180b14213abb6e5942c5c
son0925 <155799653+son0925@users.noreply.github.com>
2024-07-28 22:30:47 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.08 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,14 +1,19 @@
 class Solution {
     public String solution(int age) {
-        String alpha = "abcdefghijklmnopqrstuvwxyz";
+        // 숫자 0-9를 알파벳 a-j로 변환하기 위한 문자열 정의
+        String alpha = "abcdefghij";
         
+        // 변환된 결과를 저장할 StringBuilder 객체 생성
         StringBuilder sb = new StringBuilder();
         
+        // age가 0보다 큰 동안 반복
         while (age > 0) {
-            sb.append(alpha.charAt(age%10));
-            age /= 10;
+            // age의 마지막 자리 숫자를 알파벳으로 변환하여 StringBuilder의 앞에 삽입
+            sb.insert(0, alpha.charAt(age % 10));
+            age /= 10; // age를 10으로 나누어 자리수를 제거
         }
         
-        return sb.reverse().toString();
+        // 변환된 문자열을 반환
+        return sb.toString();
     }
 }
