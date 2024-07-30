317ebebc1d1e2b0ec7a95054231d7431b395de59
son0925 <155799653+son0925@users.noreply.github.com>
2024-07-30 16:04:38 +0900

    [level 1] Title: 숫자 짝꿍, Time: 57.87 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,35 +1,29 @@
-import java.util.*;
-
 class Solution {
     public String solution(String X, String Y) {
         StringBuilder sb = new StringBuilder();
         
-        HashMap<Character,Integer> mapX = new HashMap<>();
-        HashMap<Character,Integer> mapY = new HashMap<>();
+        int[] x = new int[10];
+        int[] y = new int[10];
         
         for (int i = 0; i < X.length(); i++) {
-            mapX.merge(X.charAt(i), 1, Integer::sum);
+            x[X.charAt(i) - '0']++;
         }
         
         for (int i = 0; i < Y.length(); i++) {
-            mapY.merge(Y.charAt(i), 1, Integer::sum);
+            y[Y.charAt(i) - '0']++;
         }
         
-        List<Character> keySet = new ArrayList<>(mapX.keySet());
-        keySet.sort((o1, o2) -> o2 - o1);
-        
-        for (char key : keySet) {
-            int count = Math.min(mapX.get(key), mapY.getOrDefault(key, 0));
-            for (int i = 0; i < count; i++) {
-                sb.append(key);
+        for (int i = 9; i >= 0; i--) {
+            int count = Math.min(x[i], y[i]);
+            for (int j = 0; j < count; j++) {
+                sb.append(i);
             }
         }
         
         if (sb.length() == 0) {
             return "-1";
         }
-        
-        if (sb.charAt(0) == '0') {
+        else if (sb.charAt(0) == '0') {
             return "0";
         }
         return sb.toString();
