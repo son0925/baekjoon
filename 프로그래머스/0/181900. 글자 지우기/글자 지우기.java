8a00f5463ef12ba2dd1207ff62b8bad5606e69f2
son0925 <155799653+son0925@users.noreply.github.com>
2024-08-02 22:29:54 +0900

    [level 0] Title: 글자 지우기, Time: 0.36 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,20 +1,18 @@
-import java.util.*;
-
 class Solution {
+
     public String solution(String my_string, int[] indices) {
-        Arrays.sort(indices);
-        
         StringBuilder sb = new StringBuilder();
-        
-        int index = 0;
-        for (int i = 0; i < my_string.length(); i++) {
-            if (index < indices.length && i == indices[index]) {
-                index++;
-            } else {
-                sb.append(my_string.charAt(i));
-            }
+        String[] arr = my_string.split("");
+
+        // 주어진 인덱스 위치의 문자를 빈 문자열로 변경
+        for (int i = 0; i < indices.length; i++) {
+            arr[indices[i]] = "";
         }
-        
+
+        // 배열에서 빈 문자열이 아닌 문자를 결합하여 결과 문자열 생성
+        for (String x : arr) {
+            sb.append(x);
+        }
         return sb.toString();
     }
 }
