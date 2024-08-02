3e7920428453c3efdcfb0098c1dff78d2499903a
son0925 <155799653+son0925@users.noreply.github.com>
2024-08-02 17:27:26 +0900

    [level 3] Title: 단어 변환, Time: 0.47 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,46 +1,49 @@
 import java.util.*;
 
 class Solution {
-
+    
     static int n;
-    static int answer;
+    static int result = Integer.MAX_VALUE;
+    static String t;
+    static boolean[] visited;
+    static String[] wordsArr;
     
     public int solution(String begin, String target, String[] words) {
-        answer = Integer.MAX_VALUE;
-
-        // words 배열에 target이 존재하지 않는다면 바로 리턴하기
-        if (!Arrays.asList(words).contains(target)) {
-            return 0;
-        }
+        n = target.length();
+        t = target;
+        wordsArr = words;
+        visited = new boolean[words.length];
         
-        n = begin.length();
-        boolean[] visited = new boolean[words.length];
-        dfs(begin, target, words, visited, 0);
-        return answer == Integer.MAX_VALUE ? 0 : answer;
+        if (Arrays.asList(words).contains(target)) {
+            dfs(begin, 0);
+            return result;
+        }
+        return 0;
     }
-
-    static void dfs(String currentWord, String target, String[] words, boolean[] visited, int result) {
-        if (currentWord.equals(target)) {
-            answer = Math.min(result, answer);
+    
+    static void dfs(String currentWord, int count){
+        if (currentWord.equals(t)) {
+            result = Math.min(result, count);
             return;
         }
         
-        for (int i = 0; i < words.length; i++) {
-            if (!visited[i] && canTransform(currentWord, words[i])) {
+        for (int i = 0; i < wordsArr.length; i++) {
+            if (!visited[i] && canTransForm(wordsArr[i], currentWord)) {
                 visited[i] = true;
-                dfs(words[i], target, words, visited, result + 1);
-                visited[i] = false; 
+                dfs(wordsArr[i], count+1);
+                visited[i] = false;
             }
         }
     }
     
-    static boolean canTransform(String word1, String word2) {
+    static boolean canTransForm(String word1, String word2) {
         int count = 0;
         for (int i = 0; i < n; i++) {
-            if (word1.charAt(i) != word2.charAt(i)) {
+            if (word1.charAt(i) == word2.charAt(i))
                 count++;
-            }
         }
-        return count == 1;
+        
+        return count == n-1;
     }
+    
 }
