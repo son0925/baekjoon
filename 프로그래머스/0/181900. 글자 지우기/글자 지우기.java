6e3f08343c4dac3a7e7187241b3f0bca02faf57e
son0925 <155799653+son0925@users.noreply.github.com>
2024-08-02 22:23:30 +0900

    [level 0] Title: 글자 지우기, Time: 0.62 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        Arrays.sort(indices);
+        
+        StringBuilder sb = new StringBuilder();
+        
+        int index = 0;
+        for (int i = 0; i < my_string.length(); i++) {
+            if (index < indices.length && i == indices[index]) {
+                index++;
+            } else {
+                sb.append(my_string.charAt(i));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
