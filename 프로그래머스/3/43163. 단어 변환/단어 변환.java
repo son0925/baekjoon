c77e21ca3754828d8ae4fc00c19d1adc4beacc95
son0925 <155799653+son0925@users.noreply.github.com>
2024-08-02 17:45:59 +0900

    [level 3] Title: 단어 변환, Time: 2.32 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,49 +1,52 @@
 import java.util.*;
 
 class Solution {
-    
-    static int n;
-    static int result = Integer.MAX_VALUE;
-    static String t;
-    static boolean[] visited;
-    static String[] wordsArr;
-    
-    public int solution(String begin, String target, String[] words) {
-        n = target.length();
-        t = target;
-        wordsArr = words;
-        visited = new boolean[words.length];
+    class Word {
+        String word;
+        int count;
         
-        if (Arrays.asList(words).contains(target)) {
-            dfs(begin, 0);
-            return result;
+        public Word(String word, int count) {
+            this.word = word;
+            this.count = count;
         }
-        return 0;
     }
-    
-    static void dfs(String currentWord, int count){
-        if (currentWord.equals(t)) {
-            result = Math.min(result, count);
-            return;
-        }
+    public int solution(String begin, String target, String[] words) {
+        Queue<Word> q = new LinkedList<>();
+        q.add(new Word(begin, 0));
+        boolean[] visited = new boolean[words.length];
+        int result = Integer.MAX_VALUE;
         
-        for (int i = 0; i < wordsArr.length; i++) {
-            if (!visited[i] && canTransForm(wordsArr[i], currentWord)) {
-                visited[i] = true;
-                dfs(wordsArr[i], count+1);
-                visited[i] = false;
+        while(!q.isEmpty()) {
+            Word word = q.poll();
+            String currentWord = word.word;
+            int count = word.count;
+            
+            
+            if (currentWord.equals(target)) {
+                result = Math.min(result, count);
+            } else {
+                for (int i = 0; i < words.length; i++) {
+                    if (!visited[i] && isTransForm(currentWord, words[i])) {
+                        visited[i] = true;
+                        q.add(new Word(words[i], count+1));
+                    }
+                }
             }
         }
+        if (result == Integer.MAX_VALUE) {
+            return 0;
+        }
+        return result;
     }
     
-    static boolean canTransForm(String word1, String word2) {
+    static boolean isTransForm(String word1, String word2) {
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (word1.charAt(i) == word2.charAt(i))
-                count++;
+        for (int i = 0; i < word1.length(); i++) {
+            if (word1.charAt(i) != word2.charAt(i)) {
+                if (++count > 1) return false;
+            }
         }
         
-        return count == n-1;
+        return true;
     }
-    
 }
