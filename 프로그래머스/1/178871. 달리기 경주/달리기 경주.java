1164d85ed9c9bdcd8f97001474b137c15cbb83f1
son0925 <155799653+son0925@users.noreply.github.com>
2024-08-02 15:44:46 +0900

    [level 1] Title: 달리기 경주, Time: 319.17 ms, Memory: 362 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -2,30 +2,34 @@ import java.util.*;
 
 class Solution {
     static HashMap<String,Integer> map = new HashMap<>();
+
     public String[] solution(String[] players, String[] callings) {
-        
-        // key : 플레이어이름, value : 순위
+        // 각 플레이어의 초기 순위를 저장할 HashMap 초기화
         for (int i = 0; i < players.length; i++) {
             map.put(players[i], i);
         }
-        
+
+        // callings 배열을 순회하며 호출된 플레이어의 순위를 변경
         for (String calling : callings) {
+            // 호출된 플레이어의 현재 순위를 map에서 가져옴
             int idx = map.get(calling);
+            // 플레이어 위치 교환 메소드 호출
             playerSwap(players, idx);
         }
-        
+
+        // 최종적으로 변경된 players 배열 반환
         return players;
     }
     
-    
+    // 플레이어의 순위를 교환하는 메소드
     static void playerSwap(String[] players, int idx) {
-        // players 배열 위치 변경
-        String tmp = players[idx-1];
-        players[idx-1] = players[idx];
+        // 호출된 플레이어와 그 앞 플레이어의 위치를 변경
+        String tmp = players[idx - 1];
+        players[idx - 1] = players[idx];
         players[idx] = tmp;
-        
-        // map 변경하기
+
+        // map에서 각 플레이어의 순위를 업데이트
         map.put(players[idx], idx);
-        map.put(players[idx-1], idx-1);
+        map.put(players[idx - 1], idx - 1);
     }
 }
