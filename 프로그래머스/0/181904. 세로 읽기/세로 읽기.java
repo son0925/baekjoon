7ec19c789101404373583f2f5b08c8755c2005a7
son0925 <155799653+son0925@users.noreply.github.com>
2024-08-02 18:38:50 +0900

    [level 0] Title: 세로 읽기, Time: 14.03 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -1,11 +1,12 @@
 class Solution {
     public String solution(String my_string, int m, int c) {
-        String result = "";
+        String result = "";  // 결과 문자열을 저장할 변수
         
-        for (int i = c-1; i < my_string.length(); i+=m) {
-            result += my_string.charAt(i);
+        // c-1부터 시작하여 m 간격으로 반복문을 수행
+        for (int i = c-1; i < my_string.length(); i += m) {
+            result += my_string.charAt(i);  // 문자열에서 해당 인덱스의 문자 추가
         }
         
-        return result;
+        return result;  // 결과 문자열 반환
     }
 }
