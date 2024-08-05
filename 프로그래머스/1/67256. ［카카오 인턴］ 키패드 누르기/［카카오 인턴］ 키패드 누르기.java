c71a8577e56e36b93b0f7c538bef2eddabcca9fa
son0925 <155799653+son0925@users.noreply.github.com>
2024-08-05 20:28:42 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 1.35 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,59 +1,51 @@
 import java.util.*;
 
 class Solution {
+    
+    static int[] leftPos = {3,0};
+    static int[] rightPos = {3,2};
+    static boolean isRight;
+    static HashMap<Integer,int[]> map;
+    
     public String solution(int[] numbers, String hand) {
         StringBuilder sb = new StringBuilder();
-        HashMap<Integer,int[]> map = mapSetting();
-        
-        int[] leftIndex = {3,0};
-        int[] rightIndex = {3,2};
+        // 번호 위치 설정
+        map = numbersPosition();
+        // 손 설정
+        isRight = hand.equals("right");
         
         for (int n : numbers) {
-            int[] indexArr = map.get(n);
-            int x = indexArr[0];
-            int y = indexArr[1];
-            if (y == 0) {
-                sb.append("L");
-                leftIndex = indexArr;
-            }
-            else if (y == 2) {
-                sb.append("R");
-                rightIndex = indexArr;
-            }
-            // 손가락 위치에 따라, 주로 사용하는 손에 따라 결정하기
-            else {
-                int lx = leftIndex[0];
-                int ly = leftIndex[1];
-                int rx = rightIndex[0];
-                int ry = rightIndex[1];
-                
-                int lIndex = Math.abs(lx - x) + Math.abs(ly - y);
-                int rIndex = Math.abs(rx - x) + Math.abs(ry - y);
-                
-                if (lIndex < rIndex) {
-                    sb.append("L");
-                    leftIndex = indexArr;
-                }
-                else if (rIndex < lIndex) {
-                    sb.append("R");
-                    rightIndex = indexArr;
-                } else {
-                    if (hand.equals("right")) {
-                        sb.append("R");
-                        rightIndex = indexArr;
-                    } else {
-                        sb.append("L");
-                        leftIndex = indexArr;
-                    }
-                }
-            }
+            int[] pos = map.get(n);
+            
+            String s = handPos(pos);
+            
+            if (s.equals("R")) rightPos = pos;
+            else leftPos = pos;
+            sb.append(s);
         }
-        
         
         return sb.toString();
     }
     
-    static HashMap<Integer,int[]> mapSetting() {
+    static String handPos(int[] pos) {
+        if (pos[1] == 0) return "L";
+        else if (pos[1] == 2) return "R";
+        
+        if (getDist(pos)) return "R";
+        else return "L";
+    }
+    
+    static boolean getDist(int[] pos) {
+        int leftDist = Math.abs(pos[0] - leftPos[0]) + Math.abs(pos[1] - leftPos[1]);
+        int rightDist = Math.abs(pos[0] - rightPos[0]) + Math.abs(pos[1] - rightPos[1]);
+        
+        if (leftDist > rightDist) return true;
+        else if (leftDist < rightDist) return false;
+        
+        return isRight;
+    }
+    
+    static HashMap<Integer,int[]> numbersPosition() {
         int[][] numbersArr = {{1,2,3},{4,5,6},{7,8,9},{-1,0,-1}};
         HashMap<Integer,int[]> map = new HashMap<>();
         for (int i = 0; i < numbersArr.length; i++) {
