ecdcf259c67014e1eea427d12bfb80bb751a0c2c
son0925 <155799653+son0925@users.noreply.github.com>
2024-08-05 21:47:30 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.94 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -2,44 +2,45 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        // 정사각형 크기 n * n
+        // 정사각형 보드의 크기
         int n = board.length;
         
-        // 터트려져 사라진 인형 수
+        // 터트려져 사라진 인형의 수를 저장할 변수
         int result = 0;
         
-        // 인형을 담을 바구니
+        // 인형을 담을 바구니 (Stack 자료구조 사용)
         Stack<Integer> stack = new Stack<>();
         
-        // 크래인 작동하기 moves배열을 순서대로 실행하며 line -1을 하여 배열에 접근하기
+        // 크레인의 움직임을 나타내는 moves 배열을 순회
         for (int line : moves) {
+            // 각 열(column)의 맨 위부터 인형을 찾기 위해 순회
             for (int i = 0; i < n; i++) {
-                // 0 : 비어있을 때 continue;
-                if (board[i][line-1] == 0) continue;
+                // 보드의 해당 위치가 비어있으면 (0이면) 다음 위치로 이동
+                if (board[i][line - 1] == 0) continue;
                 
-                // 0이 아니며 stack이 비어있을 때 추가하기
+                // 보드의 해당 위치가 비어있지 않으면 (인형이 있으면)
+                // 스택이 비어있을 때: 인형을 바구니에 추가
                 if (stack.isEmpty()) {
-                    stack.push(board[i][line-1]);
-                }
-                
-                // 0이 아니며 stack에 요소가 있을 때 제일 위에 있는 인형이 같은거라면 터뜨리고 + 2하기(인형이 2개이기 때문에)
-                else if (stack.peek() == board[i][line-1]) {
-                    System.out.println("stack : " + stack.peek() + " borad : " + board[i][line-1]);
+                    stack.push(board[i][line - 1]);
+                } 
+                // 스택이 비어있지 않을 때: 바구니 맨 위의 인형과 현재 인형 비교
+                else if (stack.peek() == board[i][line - 1]) {
+                    // 동일한 인형이면 바구니에서 제거하고 사라진 인형 수를 증가
                     stack.pop();
-                    result += 2;
-                }
-                
-                // 0이 아니며 stack에 요소가 있을 때 제일 위에 있는 인형이 다르다면 추가하기
+                    result += 2; // 인형 두 개가 사라지므로 +2
+                } 
+                // 바구니 맨 위의 인형과 현재 인형이 다르면 바구니에 추가
                 else {
-                    stack.push(board[i][line-1]);
+                    stack.push(board[i][line - 1]);
                 }
                 
-                // 인형을 뽑았기 때문에 0으로 바꾸고 다음 moves 실행
-                board[i][line-1] = 0;
-                break;
+                // 인형을 뽑았으므로 해당 위치를 0으로 변경
+                board[i][line - 1] = 0;
+                break; // 다음 moves 실행
             }
         }
         
+        // 최종적으로 사라진 인형의 수 반환
         return result;
     }
 }
