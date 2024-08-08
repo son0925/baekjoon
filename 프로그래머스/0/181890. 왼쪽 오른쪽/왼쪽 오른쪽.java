ca7c498c9d60fc037690ffa6cf5cc53675b7ebae
son0925 <155799653+son0925@users.noreply.github.com>
2024-08-08 14:32:40 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -2,28 +2,14 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] str_list) {
-        List<String> list = new ArrayList<>();
-        
-        int s = 0;
-        int e = 0;
         for (int i = 0; i < str_list.length; i++) {
             if (str_list[i].equals("l")) {
-                e = i;
-                break;
+                return Arrays.copyOfRange(str_list, 0, i);
             } else if (str_list[i].equals("r")) {
-                s = i + 1;
-                e = str_list.length;
-                break;
+                return Arrays.copyOfRange(str_list, i+1, str_list.length);
             }
         }
         
-        String[] result = new String[e-s];
-        
-        int index = 0;
-        for (int i = s; i < e; i++) {
-            result[index++] = str_list[i];
-        }
-        
-        return result;
+        return new String[0];
     }
 }
