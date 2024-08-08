31b3f665333b6111578b3c11a895801dfba63f4d
son0925 <155799653+son0925@users.noreply.github.com>
2024-08-08 14:13:03 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 21.25 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -2,18 +2,16 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String myString) {
-        List<String> list = new ArrayList<>();
-        for (String s : myString.split("x")) {
-            if (!s.equals("")) {
-                list.add(s);
-            }
+        StringTokenizer st = new StringTokenizer(myString, "x");
+        int n = st.countTokens();
+        String[] result = new String[n];
+        
+        for (int i = 0; i < n; i++) {
+            result[i] = st.nextToken();
         }
         
-        String[] result = new String[list.size()];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = list.get(i);
-        }
         Arrays.sort(result);
+        
         return result;
     }
 }
