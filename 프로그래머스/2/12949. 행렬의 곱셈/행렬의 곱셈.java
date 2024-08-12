7aa61cb0a8f2abcbfb592703b9b2bfa052fde712
son0925 <155799653+son0925@users.noreply.github.com>
2024-08-12 18:44:52 +0900

    [level 2] Title: 행렬의 곱셈, Time: 5.97 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,25 +1,28 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
+        // 결과 행렬의 크기는 arr1의 행 개수와 arr2의 열 개수에 맞춥니다.
         int[][] result = new int[arr1.length][arr2[0].length];
-        
+
+        // 결과 행렬의 각 요소를 계산합니다.
         for (int i = 0; i < result.length; i++) {
             for (int j = 0; j < result[i].length; j++) {
                 result[i][j] = matrixMultiplication(arr1, arr2, i, j);
             }
         }
-        
+
         return result;
     }
-    
+
     public static int matrixMultiplication(int[][] arr1, int[][] arr2, int i, int j) {
         int result = 0;
-        
-        int index = 0;
-        while (index < arr1[0].length || index < arr2.length) {
+
+        // arr1의 열의 개수와 arr2의 행의 개수는 같아야 합니다.
+        // 이 조건이 맞는 경우, 다음의 연산을 수행합니다.
+        for (int index = 0; index < arr1[0].length; index++) {
             result += arr1[i][index] * arr2[index][j];
-            index++;
         }
-        
-        return result;
+
+    return result;
     }
+
 }
