2d75769932c44df0c9e3b534cd25309e2a16f284
son0925 <155799653+son0925@users.noreply.github.com>
2024-08-12 17:16:16 +0900

    [level 2] Title: H-Index, Time: 17.27 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,23 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
+        Arrays.sort(citations);
         int result = 0;
         int n = citations.length;
-        Arrays.sort(citations);
-        int maxNum = citations[n-1];
         
-        for (int h = maxNum; h > 0; h--) {
-            int count = 0;
-            for (int i = 0; i < n; i++) {
-                if (citations[i] >= h) {
-                    count++;
-                }
-            }
-            
-            if (count >= h && n - count <= h) {
-                result = h;
-                return result;
-            }
+        for (int i = 0; i < n; i++) {
+            int min = Math.min(citations[i], n - i);
+            result = Math.max(result, min);
         }
         
         return result;
