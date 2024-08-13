a0bd5745c2fac7b5fb7f6246da25d9e803edc4c5
son0925 <155799653+son0925@users.noreply.github.com>
2024-08-13 15:38:14 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.19 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
@@ -1,12 +1,15 @@
 class Solution {
     public int solution(String myString, String pat) {
-        int result = 0;
-        
-        for (int i = 0; i < myString.length()-pat.length()+1; i++) {
-            if (myString.substring(i, pat.length() + i).equals(pat)) 
-                result++;
+        int result = 0; // 패턴이 발견된 횟수를 저장하는 변수
+        int n = pat.length(); // 패턴의 길이를 저장하는 변수
+
+        // 문자열 myString의 각 위치에서 패턴 pat의 길이만큼 부분 문자열을 추출하여 비교합니다.
+        for (int i = 0; i < myString.length() - n + 1; i++) {
+            // i번째 위치부터 시작해서 n개의 문자를 잘라낸 부분 문자열이 패턴과 동일한지 확인합니다.
+            if (myString.substring(i, n + i).equals(pat)) 
+                result++; // 패턴이 발견되면 카운트를 증가시킵니다.
         }
         
-        return result;
+        return result; // 패턴이 발견된 총 횟수를 반환합니다.
     }
 }
