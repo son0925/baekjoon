164cbd12eefbc44b78773704ea269757376d140e
son0925 <155799653+son0925@users.noreply.github.com>
2024-08-13 17:57:03 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 43.14 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        int[] result = new int[k];
+        List<Integer> list = new ArrayList<>();
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (list.indexOf(arr[i]) == -1 && index < k) {
+                result[index++] = arr[i];
+                list.add(arr[i]);
+            }
+        }
+        if (index < k) {
+            for (int i = index; i < k; i++) {
+                result[i] = -1;
+            }
+        }
+        return result;
+    }
+}
