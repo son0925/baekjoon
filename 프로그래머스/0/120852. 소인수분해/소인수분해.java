30a2448e9e029cc081e44f790b64de99b8428518
son0925 <155799653+son0925@users.noreply.github.com>
2024-08-15 22:14:54 +0900

    [level 0] Title: 소인수분해, Time: 0.28 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -2,26 +2,25 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        Set<Integer> set = new HashSet<>();
+        List<Integer> list = new ArrayList<>();
         
         while (n > 1) {
             for (int i = 2; i <= n; i++) {
                 if (n % i == 0) {
-                    set.add(i);
                     n /= i;
+                    if (!list.contains(i)) {
+                        list.add(i);
+                    }
                     break;
                 }
             }
         }
         
-        int[] result = new int[set.size()];
-        int index = 0;
-        Iterator iter = set.iterator();
-        while (iter.hasNext()) {
-            result[index++] = Integer.parseInt("" + iter.next());
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
         }
         
-        Arrays.sort(result);
         return result;
     }
 }
