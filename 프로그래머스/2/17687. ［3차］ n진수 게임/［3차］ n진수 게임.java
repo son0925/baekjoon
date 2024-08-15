50a3d7c94ae014e303a005ef56dcc44b854411a0
son0925 <155799653+son0925@users.noreply.github.com>
2024-08-15 17:07:43 +0900

    [level 2] Title: [3차] n진수 게임, Time: 22.35 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,44 +1,48 @@
 class Solution {
     public String solution(int n, int t, int m, int p) {
-        StringBuilder sb = new StringBuilder();
-
-        int nowNumber = 0;
-        int turn = 0;
-        while (sb.length() < t) {
-            String num = getNumberOfNthDigit(n, nowNumber);
-            for (char c : num.toCharArray()) {
-                if (sb.length() >= t) {
-                    break;
+        StringBuilder result = new StringBuilder();
+        
+        int currentNumber = 0;
+        int currentTurn = 0;
+        
+        while (result.length() < t) {
+            String convertedNumber = convertedNumberToBase(currentNumber, n);
+            for (char c : convertedNumber.toCharArray()) {
+                if (currentTurn == p - 1) {
+                    result.append(c);
                 }
-                if (turn == p - 1) { // p번째 플레이어의 차례가 맞는지 확인
-                    sb.append(c);
+                
+                if (result.length() == t) {
+                    return result.toString();
                 }
-                turn = (turn + 1) % m;
+                currentTurn = (currentTurn + 1) % m;
             }
-            nowNumber++;
+            currentNumber++;
         }
-
-        return sb.toString();
+        
+        return result.toString();
     }
-
-    static String getNumberOfNthDigit(int base, int num) {
-        if (num == 0) {
-            return "0"; // num이 0인 경우를 처리
+    
+    
+    static String convertedNumberToBase(int number, int base) {
+        StringBuilder result = new StringBuilder();
+        String[] alpha = {"A","B","C","D","E","F"};
+        if (number == 0) {
+            return "0";
         }
-
-        StringBuilder sb = new StringBuilder();
-        String[] alpha = {"A", "B", "C", "D", "E", "F"};
-
-        while (num > 0) {
-            int n = num % base;
-            if (n >= 10) {
-                sb.append(alpha[n - 10]); // 10 이상인 경우 A-F로 변환
+        
+        while (number > 0) {
+            int remainder = number % base;
+            
+            if (remainder >= 10) {
+                result.append(alpha[remainder - 10]);
             } else {
-                sb.append(n);
+                result.append(remainder);
             }
-            num = num / base;
+            
+            number = number / base;
         }
-
-        return sb.reverse().toString();
+        
+        return result.reverse().toString();
     }
 }
