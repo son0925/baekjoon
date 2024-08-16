d288397cc46b7b2286ad65d4cf9773f35c08ab0e
son0925 <155799653+son0925@users.noreply.github.com>
2024-08-16 18:04:48 +0900

    [level 0] Title: 등수 매기기, Time: 0.41 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[][] score) {
+        Double[] avgScore = new Double[score.length];
+        int n = score[0].length;    // 과목 수
+        
+        for (int i = 0; i < score.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum += score[i][j];
+            }
+            avgScore[i] = sum * 1. / n;
+        }
+        
+        int[] result = new int[score.length];
+        List<Double> list = new ArrayList<>(Arrays.asList(avgScore));
+        Collections.sort(list);
+        
+        for (int i = 0; i < avgScore.length; i++) {
+            result[i] = avgScore.length - list.lastIndexOf(avgScore[i]);
+        }
+        
+        return result;
+        
+    }
+}
