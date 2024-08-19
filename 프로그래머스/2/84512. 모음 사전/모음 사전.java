095439e63a0ca9d0fbb8fd13c36a7be1ecb9d1df
son0925 <155799653+son0925@users.noreply.github.com>
2024-08-19 16:17:43 +0900

    [level 2] Title: 모음 사전, Time: 55.08 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,45 +1,21 @@
+import java.util.*;
+
 class Solution {
+    
+    public static List<String> list = new ArrayList<>();
+    
     public int solution(String word) {
-        StringBuilder correntWord = new StringBuilder();
-        word = word.replaceAll("A", "1");
-        word = word.replaceAll("E", "2");
-        word = word.replaceAll("I", "3");
-        word = word.replaceAll("O", "4");
-        word = word.replaceAll("U", "5");
-        
-        int result = 0;
-        int count = 10;
-        while (!correntWord.toString().equals(word)) {
-            result++;
-            
-            // 길이가 5가 아니라면 A를 계속 추가하기
-            if (correntWord.length() < 5) {
-                correntWord.append("1");
-                continue;
-            }
-            
-            // 단어 바꾸기
-            char[] charArr = correntWord.toString().toCharArray();
-            for (int i = 4; i >= 0; i--) {
-                charArr[i] = (char) (charArr[i]+1);
-                
-                if (charArr[i] < '6') {
-                    break;
-                } else {
-                    charArr[i] = '0';
-                }
-            }
-            correntWord.setLength(0);
-            
-            for (int i = 0; i < 5; i++) {
-                if (charArr[i] != '0') {
-                    correntWord.append(charArr[i]);
-                } else {
-                    break;
-                }
-            }
+        dfs("");
+        return list.indexOf(word);
+    }
+    
+    public static void dfs(String s) {
+        if (s.length() > 5) {
+            return;
         }
-        
-        return result;
+        list.add(s);
+        for (int i = 0; i < 5; i++) {
+            dfs(s + "AEIOU".charAt(i));
+        }
     }
 }
