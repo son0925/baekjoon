40ebef0ee52e194e099f6bd7c8e4c0aa159593e7
son0925 <155799653+son0925@users.noreply.github.com>
2024-09-05 09:51:12 +0900

    [level 3] Title: 단속카메라, Time: 9.17 ms, Memory: 58.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int result = 0;
+        Arrays.sort(routes, (o1,o2) -> {
+            return o1[1] - o2[1];
+        });
+        
+        result++;
+        int max = routes[0][1];
+        for (int[] route : routes) {
+            if (max < route[0]) {
+                max = route[1];
+                result++;
+            }
+        }
+        return result;
+    }
+}
