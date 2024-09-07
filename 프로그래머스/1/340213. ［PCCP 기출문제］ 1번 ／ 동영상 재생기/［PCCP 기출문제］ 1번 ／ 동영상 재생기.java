e20dbc57cad0d8c0a01467fb947faec98bfedcca
son0925 <155799653+son0925@users.noreply.github.com>
2024-09-07 18:49:17 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 17.82 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -2,99 +2,69 @@ class Solution {
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
         Video video = new Video(video_len, pos, op_start, op_end);
         
-        for (int i = 0; i < commands.length; i++) {
-            video.move(commands[i].equals("next"));
+        for (String command : commands) {
+            video.move(command);
         }
+        
         return video.toString();
     }
 }
 
 class Video {
-    int[] videoLen = new int[2];
-    int[] curTime = new int[2];
-    int[] opStart = new int[2];
-    int[] opEnd = new int[2];
+    int vdTime;
+    int curTime;
+    int opStart;
+    int opEnd;
     
-    public Video(String videoLen, String pos, String opStart, String opEnd) {
-        String[] tmp = videoLen.split(":");
-        this.videoLen[0] = Integer.parseInt(tmp[0]);
-        this.videoLen[1] = Integer.parseInt(tmp[1]);
+    public Video(String video_len, String pos, String op_start, String op_end) {
+        this.vdTime = Integer.parseInt(video_len.split(":")[0]) * 60 + 
+                        Integer.parseInt(video_len.split(":")[1]);
         
-        tmp = pos.split(":");
-        this.curTime[0] = Integer.parseInt(tmp[0]);
-        this.curTime[1] = Integer.parseInt(tmp[1]);
+        this.curTime = Integer.parseInt(pos.split(":")[0]) * 60 + 
+                        Integer.parseInt(pos.split(":")[1]);
         
-        tmp = opStart.split(":");
-        this.opStart[0] = Integer.parseInt(tmp[0]);
-        this.opStart[1] = Integer.parseInt(tmp[1]);
+        this.opStart = Integer.parseInt(op_start.split(":")[0]) * 60 + 
+                        Integer.parseInt(op_start.split(":")[1]);
         
-        tmp = opEnd.split(":");
-        this.opEnd[0] = Integer.parseInt(tmp[0]);
-        this.opEnd[1] = Integer.parseInt(tmp[1]);
+        this.opEnd = Integer.parseInt(op_end.split(":")[0]) * 60 + 
+                        Integer.parseInt(op_end.split(":")[1]);
         
         opSkip();
     }
     
-    public void move(boolean next) {
-        if (next) {
-            curTime[1] += 10;
-        } else {
-            curTime[1] -= 10;
-        }
+    void opSkip() {
+        if (opStart <= curTime && curTime < opEnd)
+            curTime = opEnd;
+    }
+    
+    void move(String command) {
+        if (command.equals("next"))
+            curTime += 10;
+        else
+            curTime -= 10;
+        
         timeSet();
     }
     
-    public void timeSet() {
-        // 시간 포멧 맞추기
-        if (curTime[1] >= 60) {
-            curTime[1] -= 60;
-            curTime[0] += 1;
-        } else if (curTime[1] < 0) {
-            curTime[0] -= 1;
-            curTime[1] += 60;
-        }
-        // 비디오 길이 초과인지
-        int time = getTime(curTime);
-        int endTime = getTime(videoLen);
-        if (time > endTime) {
-            curTime[0] = videoLen[0];
-            curTime[1] = videoLen[1];
-        }
-        else if (time < 0) {
-            curTime[0] = 0;
-            curTime[1] = 0;
-        }
+    void timeSet() {
+        if (curTime < 0)
+            curTime = 0;
+        else if (curTime > vdTime)
+            curTime = vdTime;
         
-        opSkip();   
-    }
-    
-    public void opSkip() {
-        int time = getTime(curTime);
-        
-        int opStartTime = getTime(opStart);
-        int opEndTime = getTime(opEnd);
-        
-        if (opStartTime <= time && time <= opEndTime) {
-            curTime[0] = opEnd[0];
-            curTime[1] = opEnd[1];
-        }
-    }
-    
-    public int getTime(int[] arr) {
-        return arr[0] * 100 + arr[1];
+        opSkip();
     }
     
     @Override
     public String toString() {
+        String mm = "" + curTime / 60;
+        String ss = "" + curTime % 60;
         
-        String min = "" + curTime[0];
-        String sec = "" + curTime[1];
+        if (mm.length() == 1)
+            mm = "0" + mm;
+        if (ss.length() == 1)
+            ss = "0" + ss;
         
-        if (min.length() == 1)
-            min = "0" + min;
-        if (sec.length() == 1)
-            sec = "0" + sec;
-        
-        return min + ":" + sec;
+        return mm + ":" + ss;
     }
 }
