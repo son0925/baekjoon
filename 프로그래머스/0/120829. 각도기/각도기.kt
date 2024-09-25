8bc9b6606e6f6ad8c4711ea4cf83f857b2090ec5
son0925 <155799653+son0925@users.noreply.github.com>
2024-09-25 12:53:52 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.kt
+++ 프로그래머스/0/120829. 각도기/각도기.kt
@@ -2,10 +2,12 @@ class Solution {
     fun solution(angle: Int): Int {
         var result:Int = 0
         
-        if (angle < 90) result = 1
-        else if (angle == 90) result = 2
-        else if (angle < 180) result = 3
-        else result = 4
+        when(angle) {
+            in 1 until 90 -> result = 1
+            90 -> result = 2
+            in 91 until 180 -> result = 3
+            180 -> result = 4
+        }
         
         
         return result
