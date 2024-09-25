f6c796cbf31277ad2424bce43fbeee2590b2d7c0
son0925 <155799653+son0925@users.noreply.github.com>
2024-09-25 22:51:05 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.kt
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.kt
@@ -1,11 +1,5 @@
 class Solution {
     fun solution(array: IntArray, height: Int): Int {
-        var result:Int = 0
-        
-        for (i in array) {
-            if (height < i) result++
-        }
-        
-        return result
+        return array.count{it > height}
     }
 }
