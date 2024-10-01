ff5e7969a4f607ee7333db111c3c058d9710776f
son0925 <155799653+son0925@users.noreply.github.com>
2024-10-02 00:42:23 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 60.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(n: Int): Int {
+        var result = 0
+        var num = n
+        
+        while (num > 0) {
+            result += num % 10
+            num /= 10
+        }
+        
+        
+        return result
+    }
+}
