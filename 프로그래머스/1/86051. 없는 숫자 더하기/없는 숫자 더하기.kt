c5b6dfc4a8133a002d2f020dc789020aa118f2f1
son0925 <155799653+son0925@users.noreply.github.com>
2024-10-03 21:45:02 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 20.90 ms, Memory: 62.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(numbers: IntArray): Int {
+        // var result = 45
+        // numbers.map { result -= it }
+        // return result
+        
+        
+        return 45 - numbers.sum()
+    }
+}
