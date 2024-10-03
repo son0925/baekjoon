350bbbc2f81ed9a7333ca0e51951bbb8c2f7a0f0
son0925 <155799653+son0925@users.noreply.github.com>
2024-10-03 21:22:38 +0900

    [level 0] Title: 배열 원소의 길이, Time: 8.62 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.kt
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.kt
@@ -1,10 +1,12 @@
 class Solution {
     fun solution(strlist: Array<String>): IntArray {
-        val result = mutableListOf<Int>()
-        for (s in strlist) {
-            result.add(s.length)
-        }
-        return result.toIntArray()
+        // val result = mutableListOf<Int>()
+        // for (s in strlist) {
+        //     result.add(s.length)
+        // }
+        // return result.toIntArray()
         
+        
+        return strlist.map {it.length} .toIntArray()
     }
 }
