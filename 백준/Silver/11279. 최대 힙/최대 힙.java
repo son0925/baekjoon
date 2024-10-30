639509997e5087053d74763b2d69d2ec4067a39e
son0925 <155799653+son0925@users.noreply.github.com>
2024-10-30 17:39:48 +0900

    [Silver II] Title: 최대 힙, Time: 1308 ms, Memory: 113868 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Main {
+	
+	static int N; 
+	
+	public static void main(String[] args) {
+    	Scanner in = new Scanner(System.in);
+    	
+    	N = in.nextInt();
+    	
+    	PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+    	
+    	for (int i = 0; i < N; i++) {
+    		int num = in.nextInt();
+    		
+    		if (num == 0) {
+    			if (pq.isEmpty()) {
+    				System.out.println(0);
+    			} else {
+    				System.out.println(pq.poll());
+    			}
+    		} else {
+    			pq.add(num);
+    		}
+    	}
+	}
+}
