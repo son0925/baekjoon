a9d51f3cee3698e7aafe56e470e10722832f796f
son0925 <155799653+son0925@users.noreply.github.com>
2024-11-02 11:18:43 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 416 ms, Memory: 27064 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -3,59 +3,47 @@ import java.io.*;
 
 public class Main {
 	
-	static List<Integer> primeList;
+	static boolean[] prime = new boolean[10001];
 	
     public static void main(String[] args) throws IOException {
     	
-    	primeList = initPrimeList();
-        
     	Scanner in = new Scanner(System.in);
-    	
     	
     	int N = in.nextInt();
     	
+    	setPrime();
+    	
     	for (int i = 0; i < N; i++) {
     		int n = in.nextInt();
+    		int left = n / 2;
+    		int right = n / 2;
     		
-    		int result = goldbackConjecture(n);
-    		
-    		if (result > 0) {
-    			System.out.println(result + " " + (n-result));
+    		while (true) {
+    			if (prime[left] && prime[right]) {
+    				System.out.println(left + " " + right);
+    				break;
+    			}
+    			
+    			left--;
+    			right++;
     		}
     	}
     }
     
-    public static int goldbackConjecture(int n) {
-    	
-    	for (int i = n / 2; i >= 2; i--) {
-    		if (primeList.contains(i) && primeList.contains(n - i)) {
-    			return i;
-    		}
+    public static void setPrime() {
+    	for (int i = 0; i < prime.length; i++) {
+    		prime[i] = isPrime(i);
     	}
-    	return -1;
     }
-    
-    
-    public static List<Integer> initPrimeList() {
-    	List<Integer> list = new ArrayList<>();
-    	
-    	for (int i = 2; i <= 10000; i++) {
-    		if (isPrime(i)) {
-    			list.add(i);
-    		}
-    	}
-    	
-    	
-    	return list;
-    }
-    
     
     public static boolean isPrime(int num) {
+    	
     	for (int i = 2; i < (int)Math.sqrt(num)+1; i++) {
-    		if (num % i == 0) 
+    		if (num % i == 0)
     			return false;
     	}
     	
     	return true;
+    	
     }
 }
