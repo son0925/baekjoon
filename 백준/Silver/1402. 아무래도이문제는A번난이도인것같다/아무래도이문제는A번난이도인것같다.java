d987bb54c30fe305493e4cf806566c33f3bbd5c1
son0925 <155799653+son0925@users.noreply.github.com>
2024-12-01 15:31:28 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 168 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // 테스트 케이스 수
+        int T = in.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            System.out.println("yes");
+        }
+    }
+}
