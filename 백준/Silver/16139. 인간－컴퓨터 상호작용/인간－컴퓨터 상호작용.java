2b3323e5d8157ca444eb12d2ce91b8d5662d70b9
son0925 <155799653+son0925@users.noreply.github.com>
2024-12-20 12:24:27 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 796 ms, Memory: 116904 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -1,36 +1,36 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        String input = in.nextLine();
-        HashMap<Character, Integer>[] map = new HashMap[input.length()];
+        String input = br.readLine();
+        int T = Integer.parseInt(br.readLine());
 
-        map[0] = new HashMap<>();
-        map[0].put(input.charAt(0), 1);
+        int[][] dp = new int[input.length()][26];
+        dp[0][input.charAt(0) - 'a'] = 1;
         for (int i = 1; i < input.length(); i++) {
-            map[i] = new HashMap<>(map[i - 1]);
-
-            char c = input.charAt(i);
-            int currentCount = map[i].getOrDefault(c, 0);
-            map[i].put(c, currentCount + 1);
+            int alphaIdx = input.charAt(i) - 'a';
+            dp[i][alphaIdx] = 1;
+            for (int j = 0; j < 26; j++) {
+                dp[i][j] += dp[i - 1][j];
+            }
         }
-
-        int T = Integer.parseInt(in.nextLine());
 
         for (int i = 0; i < T; i++) {
-            String[] tmp = in.nextLine().split(" ");
+            String[] tmp = br.readLine().split(" ");
+            int target = tmp[0].charAt(0) - 'a';
+            int start = Integer.parseInt(tmp[1]);
+            int end = Integer.parseInt(tmp[2]);
 
-            char target = tmp[0].charAt(0);
-            int left = Integer.parseInt(tmp[1]);
-            int right = Integer.parseInt(tmp[2]);
-
-            int rightCount = map[right].getOrDefault(target, 0);
-            int leftCount = (left > 0) ? map[left - 1].getOrDefault(target, 0) : 0;
-
-            System.out.println(rightCount - leftCount);
+            if (start == 0) {
+                sb.append(dp[end][target]).append("\n");
+            } else {
+                sb.append(dp[end][target] - dp[start - 1][target]).append("\n");
+            }
         }
+
+        System.out.print(sb.toString());
     }
 }
