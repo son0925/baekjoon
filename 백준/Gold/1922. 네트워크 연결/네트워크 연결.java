ad952eab76789cb09fec78af208680d304959cd3
son0925 <155799653+son0925@users.noreply.github.com>
2024-12-30 18:33:54 +0900

    [Gold IV] Title: 네트워크 연결, Time: 432 ms, Memory: 47072 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -0,0 +1,80 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int v; // 노드 개수
+    static int e; // 간선 개수
+    static int[] parent;
+    static PriorityQueue<Edge> pq = new PriorityQueue<>();
+
+    public static void main(String[] args) throws IOException {
+        v = Integer.parseInt(br.readLine());
+        e = Integer.parseInt(br.readLine());
+
+        parent = new int[v+1];
+        for (int i = 1; i <= v; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < e; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            pq.offer(new Edge(v1, v2, weight));
+        }
+
+        int weight = 0;
+        while (!pq.isEmpty()) {
+            Edge now = pq.poll();
+
+            if (find(now.v1) != find(now.v2)) {
+                union(now.v1, now.v2);
+                weight += now.weight;
+            }
+        }
+
+        System.out.println(weight);
+    }
+
+    public static void union(int v1, int v2) {
+        int p1 = find(v1);
+        int p2 = find(v2);
+
+        if (p1 < p2) {
+            parent[p1] = p2;
+        } else {
+            parent[p2] = p1;
+        }
+    }
+
+    public static int find(int n) {
+        if (n == parent[n]) {
+            return n;
+        }
+        return parent[n] = find(parent[n]);
+    }
+}
+
+class Edge implements Comparable<Edge>{
+    int v1;
+    int v2;
+    int weight;
+
+    public Edge(int v1, int v2, int weight) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return this.weight - o.weight;
+    }
+}
