0d1556459b3cc1750eeb840fe74487b5164ea15b
son0925 <155799653+son0925@users.noreply.github.com>
2025-01-14 18:52:23 +0900

    [Bronze V] Title: 등록, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(361);
+        System.out.println("shs00925");
+    }
+}
