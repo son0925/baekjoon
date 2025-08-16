7e932787465c4d4cf8b388c556ac2fbac8900133
son0925 <155799653+son0925@users.noreply.github.com>
2025-08-16 16:57:10 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println((n / 5) + (n % 5 > 0 ? 1 : 0));
+    }
+}
